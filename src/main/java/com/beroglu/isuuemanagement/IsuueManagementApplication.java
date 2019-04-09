0e80245ab9e45973ecdb8db7914f2f25e8974f37
beroglu <berfin.malgr@gmail.com>
2019-04-09 23:56:35 +0300

    h2 düzeltildi

--- src/main/java/com/beroglu/isuuemanagement/IsuueManagementApplication.java
+++ src/main/java/com/beroglu/isuuemanagement/IsuueManagementApplication.java
@@ -1,11 +1,13 @@
 package com.beroglu.isuuemanagement;
 
+import org.h2.server.web.WebServlet;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
@@ -25,5 +27,10 @@ public class IsuueManagementApplication {
 		return modelMapper;
 
 	}
-
+	@Bean
+	public ServletRegistrationBean h2servletRegistration() {
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
+		registrationBean.addUrlMappings("/console/*");
+		return registrationBean;
+	}
 }
